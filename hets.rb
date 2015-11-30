d5c4e24ac5ae86deb41bae394238546244e91766
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-11-30 10:48:19 +0100

    Bump version to 1448876782.

--- hets.rb
+++ hets.rb
@@ -4,8 +4,8 @@ require 'rexml/document'
 class Hets < Formula
   # Both the version and the sha1 need to be adjusted when a new
   # dmg-version of hets is released.
-  @@version_commit = '4ba467d64c56a7a3856ee817323c8d521c210e48'
-  @@version_unix_timestamp = '1443428993'
+  @@version_commit = '3e5dd87f67010dcea4642e1d32a6d8fe57daf677'
+  @@version_unix_timestamp = '1448876782'
   homepage "http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/index_e.htm"
   head "https://github.com/spechub/Hets.git", :using => :git
   url "https://github.com/spechub/Hets.git", :using => :git, :revision => @@version_commit
