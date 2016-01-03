5cfd2b7cda1de5a0f60918d723f4d362586c1717
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-01-03 15:32:09 +0100

    Manage dependencies for GHC 7.10
    Some older versions of the packages are incompatible with GHC >=7.10.
    Those have been updated.
    Except programatica: It cannot be compiled with GHC >=7.10 and it is
    fairly hard to port it to this GHC version. It only allows Hets to
    support Haskell as a logic.

--- hets-dependencies.rb
+++ hets-dependencies.rb
@@ -7,20 +7,27 @@ class HetsDependencies < Formula
   url "https://gist.githubusercontent.com/0robustus1/9049050/raw/a9c8e0dcbefef624090b6d3bce279fd73c579598/install_cabal_hets_deps.sh"
   homepage 'https://gist.github.com/0robustus1/9049050'
   sha1 'cfa0ebdb51e6654ca3cea926b8e3106cda178de1'
-  version '0.1.3'
+  version '0.1.4'
 
   depends_on 'ghc'
   depends_on 'cabal-install'
   depends_on 'glib'
   depends_on 'cairo'
+  depends_on :x11
   depends_on 'gtk'
   depends_on 'gettext'
   depends_on 'fontconfig'
+  depends_on 'freetype'
 
   def install
     ghc_prefix = `ghc --print-libdir | sed -e 's+/lib.*/.*++g'`.strip
     opts = ['--force-reinstalls', '--enable-documentation', '-p', '--global', "--prefix=#{ghc_prefix}"]
-    packages = %w{
+    packages_list = [
+    %w{
+      gtk2hs-buildtools
+    }, %w{
+      gtk -fhave-quartz-gtk
+    }, %w{
       aterm
       random
       utf8-string
@@ -29,18 +36,17 @@ class HetsDependencies < Formula
       HTTP
       haskeline
       HaXml
-      hexpat-0.20.6
+      hexpat
       uni-uDrawGraph
       parsec1
-      wai-extra-3.0.2.1
-      warp-3.0.1.1
-      http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/src-distribution/programatica-1.0.0.5.tar.gz
-      tar
-    }
+      wai-extra
+      warp-3.1.12
+      tar-0.4.2.2
+    }]
     system('cabal update')
-    cabal_install(*(opts + packages))
+    packages_list.each do |packages|
+      cabal_install(*(opts + packages))
+    end
     system('ghc-pkg hide parsec1')
-    system('ghc-pkg hide programatica')
   end
-
 end
