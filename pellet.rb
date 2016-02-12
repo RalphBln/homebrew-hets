72602f8fed4c9731d6954a66d18348abae1551ee
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-02-12 15:27:36 +0100

    Bump to new pellet version, use git repository.

--- pellet.rb
+++ pellet.rb
@@ -1,46 +1,21 @@
 require "formula"
 
 class Pellet < Formula
-  # Both the version and the sha1 need to be adjusted when a new
-  # dmg-version of hets is released.
-  # version "2014-01-06"
-  # sha1 "8f7a8ee54c32e67933cb84f5c66fd08da98dd2a1"
-
+  @@version_commit = 'bb99c19f9b9abf4e2c9e65098ee3e4d624e14894'
+  the_version = '2.4.0'
   homepage 'http://clarkparsia.com/pellet'
-  head 'https://github.com/clarkparsia/pellet'
-  url "https://github.com/clarkparsia/pellet/archive/2.3.1.tar.gz"
-  version "2.3.1"
-  sha1 '7d46c04fdae8d8a15af07521c4cb033fd39385eb'
+  head 'https://github.com/Complexible/pellet.git', :using => :git
+  url 'https://github.com/Complexible/pellet.git', :using => :git, :revision => @@version_commit
+  version "#{the_version}-0-SNAPSHOT"
 
-  option 'with-script', 'includes a pellet script which is placed in PATH'
+  depends_on :java
+  depends_on 'maven'
 
   def install
-    system('ant')
-    prefix.install Dir['dist/*']
-
-    if build.with? 'script'
-      bin.mkdir
-      bin.join('pellet').open('w') do |f|
-        f.write <<-SH
-#!/bin/sh
-
-if [ -n "${JAVA_HOME}" -a -x "${JAVA_HOME}/bin/java" ]; then
-  java="${JAVA_HOME}/bin/java"
-else
-  java=java
-fi
-
-if [ -z "${pellet_java_args}" ]; then
-  pellet_java_args="-Xmx512m"
-fi
-
-exec ${java} ${pellet_java_args} -jar #{prefix.join('lib/pellet-cli.jar')} "$@"
-        SH
-      end
-    end
+    system('mvn clean install -DskipTests')
+    prefix.install Dir['cli/target/pelletcli/*']
   end
 
   def caveats
   end
-
 end
